ef16e4a6902ecc586aa7efcf4fbd2ddc0e209c1e
Nick Pope <npope@bestunion.com>
2018-09-12 16:33:35 -0400

    Items are now appended to UL, going to update to edit task

--- javascript/todo-list/vanillaJS/todo.js
+++ javascript/todo-list/vanillaJS/todo.js
@@ -3,15 +3,25 @@
 	// Begin Array.
 	var taskList = [];
 
+	// Document Variables.
+	var addTodo = document.getElementById('addTodo');
+	var ulList = document.getElementById('list');
+
 	//fn() to create task Obj.
 	function task(task, completed) {
 		this.task = task;
 		this.completed = completed;
 	}
 
-	// Variable to get value of input.
-	var addTodo = document.getElementById('addTodo');
-
+	//fn() to add list item.
+	function addLi(text, ind) {
+		var li = document.createElement('li');
+		var liText = document.createTextNode(text.task);
+		li.appendChild(liText);
+		li.setAttribute('id', ind);
+		li.setAttribute('class', 'task-item');
+		ulList.appendChild(li);
+	}
 	// Listening for "enter" key to be pressed.
 	addTodo.addEventListener("keyup", function() {
 		if(event.keyCode == 13) {
@@ -22,6 +32,8 @@
 			// Clear Value for next Input.
 			addTodo.value = '';
 			console.log('taskList ', taskList);
+			var taskIndex = taskList.length - 1;
+			addLi(newTask, taskIndex);
 		}
 	});
 
