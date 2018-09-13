fa769b02887a61147e61205c2d966885013fa9cf
Nick Pope <npope@bestunion.com>
2018-09-13 11:22:54 -0400

    Update to close edit when attempting to edit a different value

--- javascript/todo-list/vanillaJS/todo.js
+++ javascript/todo-list/vanillaJS/todo.js
@@ -2,7 +2,6 @@
 	
 	// Begin Array.
 	var taskList = [];
-	var tmpValueObj;
 
 	// Document Variables.
 	var todo = document.getElementById('addTodo');
@@ -24,38 +23,44 @@
 		list.appendChild(li);
 	}
 
-	function editTask(taskId) {
-		var edit = document.getElementById(taskId);
-		// Save variable to IIFE Scope, so that it can be used to save a value if a user 'clicks' off the input.
-		tmpValueObj = {
-			value: edit.innerText,
-			ind: taskId
-		};
-		var editTask = '<input id="inp-' + taskId + '" class="task-input task" value="' + tmpValueObj.value + '" />';
-		edit.innerHTML = editTask;
-		var newInput = document.getElementById('inp-' + taskId);
-		newInput.focus();
-	} 
-
 	function saveEdit(taskId, value) {
 		var updateTask = document.getElementById(taskId);
 		updateTask.innerHTML = value;
 		var index = parseInt(taskId);
 		taskList[index].task = value;
-
-		console.log('taskList ', taskList);
 	}
+
+	function editTask(taskId) {
+		var edit = document.getElementById(taskId);
+		// Save variable to IIFE Scope, so that it can be used to save a value if a user 'clicks' off the input.
+		var editTask = '<input id="inp-' + taskId + '" class="task-input task" value="' + tmpValueObj.value + '" />';
+		edit.innerHTML = editTask;
+		var newInput = document.getElementById('inp-' + taskId);
+		newInput.focus();
+
+		var toDoLoop = document.getElementsByClassName('task-input');
+
+		for(var i = 0; i<toDoLoop.length; i++) {
+			var tmpId = toDoLoop[i].id.replace(/[a-z]+\-/g, '');
+			var tmpVal = toDoLoop[i].value;
+			if(tmpId !== taskId) {
+				saveEdit(tmpId, tmpVal);
+			}
+		}
+		console.log('tod ', toDoLoop);
+	} 
 
 	// Listening for "enter" key to be pressed.
 	document.addEventListener("keyup", function(event) {
-		if(event.keyCode == 13) {
-			var getClassList = event.target.classList;
-			//Replace Alphanumerical Characters to get the index from the input - pattern would not affect addTodo.
-			var getId = event.target.id.replace(/[a-z]+\-/g, '');
-			var getValue = event.target.value;
+		var getClassList = event.target.classList;
+		//Replace Alphanumerical Characters to get the index from the input - pattern would not affect addTodo.
+		var getId = event.target.id.replace(/[a-z]+\-/g, '');
+		var getValue = event.target.value;
+
+		if(event.keyCode === 13) {
 			if(getId === 'addTodo') {
 
-				// Create task Onject
+				// Create task Object
 				var newTask = new task(getValue, false);
 				// Push newTask into the array (at the end).
 				taskList.push(newTask);
@@ -67,6 +72,12 @@
 				saveEdit(getId, getValue);
 			}
 
+		} else if (event.keyCode !== 13 && getClassList.contains('task-input')) {
+			// Update tmpValueObj to track changes... Maybe I should disallow this? UX question.
+			tmpValueObj = {
+				value: getValue,
+				ind: getId
+			};
 		}
 	});
 
@@ -77,20 +88,6 @@
 		if(getClassList.contains('task-item')) {
 			editTask(getId);
 		}
-	});
-
-	document.addEventListener('click', function(event) {
-		var getClassList = event.target.classList;
-		try {
-			if(!getClassList.contains('task-input')) {
-				if(typeof tmpValueObj === 'object') {
-					saveEdit(tmpValueObj.ind, tmpValueObj.value);
-				}
-			} 
-		} catch(e) {
-			console.error('Error on Click: ', e);
-		}
-
 	});
 
 })(); // End IIFE
